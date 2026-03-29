76b9178c3161fe54e16b2ad7f8049d68f8afe200
affonsobx <affonsobx@gmail.com>
2026-03-28 23:43:35 -0300

    Adicionando histórico

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function conversor() {
     let valorOriginal = document.getElementById('valorDigitado').value;
     let valorCorrigido = valorOriginal.replace(',','.') 
-
     let metros = parseFloat(valorCorrigido);
     if (metros) {
         let centimetros = metros * 100;
-        document.getElementById('resultado').innerText = centimetros.toFixed(2);
+        let resultadoFormatado = centimetros.toFixed(2);
+
+        document.getElementById('resultado').innerText = resultadoFormatado;
+        let novoItem = document.createElement("li");
+        novoItem.innerText = metros + "m = " + resultadoFormatado + "cm";
+        let lista = document.getElementById("listaHistorico");
+        lista.prepend(novoItem);
     }
 }
